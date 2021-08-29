0f3fdf7f46e986eecca5f4a7b0f00236cd92f234
Radif <mail@radif.ru>
2021-08-29 20:15:35 +0300

    Настройки приложения Авторы

---
+++ library/authors/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AuthorsConfig(AppConfig):
+    """Настройки приложения Авторы"""
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'authors'
