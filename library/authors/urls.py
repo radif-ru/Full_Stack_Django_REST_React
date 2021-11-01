d1dbe1963d067391f0a8d023344435b8e24c7bdd
Radif <mail@radif.ru>
2021-11-01 22:33:50 +0300

    Изменен url-адрес авторов

--- library/authors/urls.py
+++ library/authors/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from authors.views import AuthorModelViewSet
 
+# Роутер для авто-создания набора url-адресов (связь с id, get, set и т.д.)
 router = DefaultRouter()
-router.register('author', AuthorModelViewSet)
+router.register('authors', AuthorModelViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
