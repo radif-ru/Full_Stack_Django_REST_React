a9f412f096beb45b8ab3689006a1342f458cff27
Radif <mail@radif.ru>
2021-11-05 23:08:34 +0300

    Подключение средства визуализации в верблюжьем стиле для JSON и змеином для питона. А так же отображение, удобное для администрирования

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,6 @@
+from djangorestframework_camel_case.render import CamelCaseJSONRenderer, \
+    CamelCaseBrowsableAPIRenderer
+from rest_framework.renderers import AdminRenderer
 from rest_framework.viewsets import ModelViewSet
 from .models import Todo, Project
 from .serializers import TodoModelSerializer, ProjectModelSerializer
@@ -5,11 +8,19 @@ from .serializers import TodoModelSerializer, ProjectModelSerializer
 
 class TodoModelViewSet(ModelViewSet):
     """Набор представлений для модели заметок"""
+    renderer_classes = (
+        # Верблюжий стиль JSON и браузерного API
+        CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer
+    )
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
 
 
 class ProjectModelViewSet(ModelViewSet):
     """Набор представлений для модели проектов"""
+    renderer_classes = (
+        # Верблюжий стиль JSON и браузерного API
+        CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer
+    )
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,7 @@
+from djangorestframework_camel_case.render import CamelCaseJSONRenderer, \
+    CamelCaseBrowsableAPIRenderer
+from rest_framework.renderers import AdminRenderer, JSONRenderer, \
+    BrowsableAPIRenderer, TemplateHTMLRenderer, StaticHTMLRenderer
 from rest_framework.viewsets import ModelViewSet
 from .models import User
 from .serializers import UserModelSerializer
@@ -5,5 +9,9 @@ from .serializers import UserModelSerializer
 
 class UserModelViewSet(ModelViewSet):
     """Набор представлений для модели Пользователь"""
+    renderer_classes = (
+        # Верблюжий стиль JSON и API, и стиль удобного администрирования
+        CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer, AdminRenderer
+    )
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
