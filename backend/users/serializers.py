f014a5832b5e0aac3f898250a1fcfdd2f46f0a02
Radif <mail@radif.ru>
2021-11-05 16:21:16 +0300

    Валидация данных пользователя. Отсеиваем школоту)

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,9 +1,24 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from datetime import timedelta
+
+from django.utils import timezone
+from rest_framework import serializers
+
 from .models import User
 
 
-class UserModelSerializer(HyperlinkedModelSerializer):
-    """Сериализация модели пользователя Автор"""
+class UserModelSerializer(serializers.ModelSerializer):
+    """Сериализация модели пользователя"""
+
+    def validate(self, attrs):
+        # Разница дней между текущей датой и ДР
+        days = timezone.now().date() - attrs['birthdate']
+        # 365.2425 - в среднем дней в году по Григорианскому календарю
+        years = days / timedelta(days=365.2425)
+        if years < 18:
+            raise serializers.ValidationError(
+                'Школоте нельзя! Или укажи возраст больше 18 лет;)')
+        return attrs
+
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'middle_name',
