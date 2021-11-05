715834a854f264f6699bb79f0ee2475b449fa252
Radif <mail@radif.ru>
2021-11-05 16:17:25 +0300

    Настройка приложения users

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
 
 
-class AuthorsConfig(AppConfig):
+class UsersConfig(AppConfig):
     """Настройки приложения Пользователи"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+    verbose_name = 'пользователь'
