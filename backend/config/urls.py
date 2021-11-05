7f645fd138073e1756f0c6e05d5fc5bfd5d57746
Radif <mail@radif.ru>
2021-11-05 23:11:20 +0300

    Endpoint-ы теперь подключаются из главного файла urls.py в каталоге config

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from todo.views import ProjectModelViewSet, TodoModelViewSet
+from users.views import UserModelViewSet
+
+# Роутер для авто-создания набора url-адресов (связь с id, get, set и т.д.)
+router = DefaultRouter()
+router.register('users', UserModelViewSet)
+router.register('projects', ProjectModelViewSet)
+router.register('todos', TodoModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('rest_framework.urls')),
 
-    path('api/', include('users.urls')),
-    path('api/', include('todo.urls')),
+    path('api/', include(router.urls)),
 ]
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,2 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-
-from .views import UserModelViewSet
-
-# Роутер для авто-создания набора url-адресов (связь с id, get, set и т.д.)
-router = DefaultRouter()
-router.register('users', UserModelViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
 ]
