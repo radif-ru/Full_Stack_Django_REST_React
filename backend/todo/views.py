cc7610d9f83d3781ebb8f26c5c8a3d3dda4a6656
Radif <mail@radif.ru>
2021-11-11 18:08:24 +0300

    Кастомизация фильтров для Проекта (по части имени) и Заметок (по дате начало/конец, id/имени проекта) upd: теперь показываются только активные заметки. Переопределен метод удаления, вместо этого заметка становится неактивной

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,7 +1,12 @@
-from djangorestframework_camel_case.render import CamelCaseJSONRenderer, \
-    CamelCaseBrowsableAPIRenderer
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+
+from djangorestframework_camel_case.render import CamelCaseJSONRenderer, \
+    CamelCaseBrowsableAPIRenderer
+from django_filters import rest_framework as filters
+
 from .models import Todo, Project
 from .serializers import TodoModelSerializer, ProjectModelSerializer
 
@@ -18,15 +23,50 @@ class ProjectPageNumberPagination(PageNumberPagination):
     page_size = 10
 
 
+# Кастомизация django-filter для Проектов
+class ProjectFilter(filters.FilterSet):
+    # Фильтрация по части названия проекта
+    name = filters.CharFilter(lookup_expr='contains')
+
+    class Meta:
+        model = Project
+        fields = ['name']
+
+
+# Кастомизация django-filter для Заметок
+class TodoFilter(filters.FilterSet):
+    # Фильтрация по дате создания заметки
+    # start_date = filters.CharFilter(lookup_expr='contains')
+    created = filters.DateTimeFromToRangeFilter()
+
+    class Meta:
+        model = Todo
+        fields = ['created', 'project', 'project__name']
+
+
 class TodoModelViewSet(ModelViewSet):
     """Набор представлений для модели заметок"""
     renderer_classes = (
         # Верблюжий стиль JSON и браузерного API
         CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer
     )
-    queryset = Todo.objects.all()
+    # Вывод только активных заметок
+    queryset = Todo.objects.filter(active=1)
     serializer_class = TodoModelSerializer
+    # Подключение кастомного пагинатора
     pagination_class = TodoPageNumberPagination
+
+    # Подключение кастомного фильтра django-filter
+    filterset_class = TodoFilter
+
+    def destroy(self, request, *args, **kwargs):
+        """ Переопределения метода удаления (DELETE)
+        Вместо фактического удаления меняем активность на 0 (False)
+        """
+        instance = self.get_object()
+        instance.active = 0
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ProjectModelViewSet(ModelViewSet):
@@ -37,4 +77,7 @@ class ProjectModelViewSet(ModelViewSet):
     )
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    # Подключение кастомного пагинатора
     pagination_class = ProjectPageNumberPagination
+    # Подключение кастомного фильтра django-filter
+    filterset_class = ProjectFilter
