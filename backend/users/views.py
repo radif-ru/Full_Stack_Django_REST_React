6189bb67613938cc0057ffec141b10394b5a68b3
Radif <mail@radif.ru>
2021-11-11 18:04:36 +0300

    Кастомизация фильтра для пользователей по части имени, фамилии, отчества. upd: теперь можно искать пользователей по части от логина. fix: в фио возвращался логин, вместо имени

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,3 @@
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.mixins import ListModelMixin, UpdateModelMixin, \
@@ -10,6 +9,8 @@ from rest_framework.viewsets import GenericViewSet
 
 from djangorestframework_camel_case.render import CamelCaseJSONRenderer, \
     CamelCaseBrowsableAPIRenderer
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 
 from .models import User
 from .serializers import UserModelSerializer
@@ -21,7 +22,19 @@ class UserLimitOffsetPagination(LimitOffsetPagination):
     default_limit = 2
 
 
-class UserModelViewSet(ListModelMixin, UpdateModelMixin, RetrieveModelMixin,
+# Кастомизация django-filter для Пользователей
+class UserFilter(filters.FilterSet):
+    # Фильтрация по части имени, фамилии, отчества
+    first_name = filters.CharFilter(lookup_expr='contains')
+    last_name = filters.CharFilter(lookup_expr='contains')
+    middle_name = filters.CharFilter(lookup_expr='contains')
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'middle_name']
+
+
+class UserModelViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin,
                        GenericViewSet):
     """Набор представлений для модели Пользователь"""
     renderer_classes = (
@@ -31,21 +44,22 @@ class UserModelViewSet(ListModelMixin, UpdateModelMixin, RetrieveModelMixin,
     )
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+    # Подключение кастомного limit-offset пагинатора
     pagination_class = UserLimitOffsetPagination
 
     # Библиотека для фильтрации запросов django-filter + визуализация в API
     # В settings есть глобальные настройки, здесь можно корректировать
     filter_backends = [DjangoFilterBackend]
-    # поле по которому фильтруем с помощью библиотеки django-filter
-    filterset_fields = ['first_name', 'last_name', 'middle_name']
+    # Подключение кастомного фильтра django-filter
+    filterset_class = UserFilter
 
     # Пример фильтрации стандартным методом
     def get_queryset(self):
         login = self.request.query_params.get('login')
         if login is not None:
-            return User.objects.filter(username=login)
+            return User.objects.filter(username__contains=login)
         else:
-            return User.objects.all()
+            return self.queryset
 
     # Дополнительные Endpoint-ы
     @action(detail=False, methods=['GET'])
@@ -63,4 +77,4 @@ class UserModelViewSet(ListModelMixin, UpdateModelMixin, RetrieveModelMixin,
     def fio(self, request, pk=None):
         user = get_object_or_404(User, pk=pk)
         return Response(
-            {'fio': f'{user.username} {user.middle_name} {user.last_name}'})
+            {'fio': f'{user.first_name} {user.middle_name} {user.last_name}'})
