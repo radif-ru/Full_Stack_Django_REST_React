9cdf94883358572e6f888b6332d1e8a81c36bdb6
Radif <mail@radif.ru>
2021-11-12 03:29:34 +0300

    Роутинг по SPA

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
+
+import './assets/global.css';
+
 import reportWebVitals from './reportWebVitals';
+import {Header} from "./components/Header";
+import {Users} from "./components/Users";
+import {Projects} from "./components/Projects";
+import {Todos} from "./components/Todos";
+import {NotFound404} from "./components/NotFound404";
+import {Footer} from "./components/Footer";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <HashRouter>
+            <Header/>
+            <Routes>
+                <Route exact path='/users' element={<Users/>}/>
+                <Route exact path='/projects' element={<Projects/>}/>
+                <Route exact path='/todos' element={<Todos/>}/>
+                <Route exact path='/'
+                       element={<Navigate to='/users'/>}/>
+                <Route path='*' element={<NotFound404/>}/>
+            </Routes>
+            <Footer/>
+        </HashRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
