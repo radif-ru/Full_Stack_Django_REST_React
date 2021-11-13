a8e65ac10a21fdb12523f3a7a82026a786d60c90
Radif <mail@radif.ru>
2021-11-13 19:05:36 +0300

    реструктуризация стилей, footer прижат книзу, header зафиксирован сверху, улучшено отображение элементов

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -7,16 +7,17 @@ import logo from './logo.svg';
 export class Footer extends PureComponent {
   render() {
     return (
-      <footer>
-        <img src={logo} className='App-logo' alt='logo'/>
+      <footer className='footer'>
         <a
           className='App-link'
           href='https://radif.ru'
           target='_blank'
           rel='noopener noreferrer'
         >
-          Radif.ru
+          &copy; 2021 &reg;Radif.ru
         </a>
+        <br/>
+        <img src={logo} className='App-logo' alt='logo'/>
       </footer>
     )
   }
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -7,27 +7,27 @@ import {Link} from "react-router-dom";
 
 const Menu = () => {
   return (
-    <nav className='menu'>
-      <ul>
-        {/*Якоря (были раньше):*/}
-        <li>
-          <Link to='/users'>Пользователи</Link>
-        </li>
-        <li>
-          <Link to='/projects'>Проекты</Link>
-        </li>
-        <li>
-          <Link to='/todos'>Заметки</Link>
-        </li>
-      </ul>
-    </nav>
+      <nav className='menu'>
+        <ul>
+          {/*Якоря (были раньше):*/}
+          <li>
+            <Link to='/users'>Пользователи</Link>
+          </li>
+          <li>
+            <Link to='/projects'>Проекты</Link>
+          </li>
+          <li>
+            <Link to='/todos'>Заметки</Link>
+          </li>
+        </ul>
+      </nav>
   )
 }
 
 export class Header extends PureComponent {
   render() {
     return (
-      <header>
+      <header className='header'>
         <Menu/>
       </header>
     )
