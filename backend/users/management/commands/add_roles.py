7807dd06fa09d3fe9916701c4002b302c9dd2ded
Radif <mail@radif.ru>
2021-11-16 02:48:50 +0300

    refactoring

--- backend/users/management/commands/add_roles.py
+++ backend/users/management/commands/add_roles.py
@@ -7,16 +7,17 @@ from users.models import PermissionGroups
 
 class Command(BaseCommand):
     help = 'Создание групп разрешений'
+    roles = Roles
 
     def handle(self, *args, **options):
         """Команды на выполнение"""
-        self.add_groups()
+        self.add_groups(self.roles)
 
     @staticmethod
-    def add_groups():
+    def add_groups(roles: iter):
         try:
             permission_groups_objs = PermissionGroups.objects
-            for role in Roles:
+            for role in roles:
                 if not permission_groups_objs.filter(role=role):
                     permission_groups_objs.create(role=role)
         except OperationalError or ProgrammingError as error:
