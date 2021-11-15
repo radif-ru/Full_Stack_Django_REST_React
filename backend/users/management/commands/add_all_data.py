bda846222fc4cca24f490a4f2c819cff8dd3d1e6
Radif <mail@radif.ru>
2021-11-16 02:54:27 +0300

    upd: К единой точке создания всех дефолтных данных в БД добавились создания проектов и заметок

--- backend/users/management/commands/add_all_data.py
+++ backend/users/management/commands/add_all_data.py
@@ -5,17 +5,23 @@ from django.core.management.base import BaseCommand
 from users.management.commands.add_users import Command as AddUsers
 from users.management.commands.add_roles import Command as AddRoles
 
+from todo.management.commands.add_projects import Command as AddProjects
+from todo.management.commands.add_todos import Command as AddTodos
+
 
 class Command(BaseCommand):
     help = 'Сборка всех подготовленных данных'
 
     def handle(self, *args, **options):
         self.migrate()
-        self.migrate()
+        self.collect_static()
 
-        AddRoles.add_groups()
+        AddRoles.add_groups(AddRoles.roles)
         AddUsers.create_admin()
         AddUsers.create_users(AddUsers.users)
+
+        AddProjects.add_projects(AddProjects.projects)
+        AddTodos.add_todos(AddTodos.todos)
 
     @staticmethod
     def migrate():
