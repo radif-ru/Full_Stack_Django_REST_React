190fa52139a34fabd1c96d5a7bdcc4be5590ae80
Radif <mail@radif.ru>
2021-11-17 01:07:52 +0300

    create: подключение к меню кнопки для авторизации/деавторизации в зависимости от состояния пользователя

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -5,22 +5,27 @@ import React, {PureComponent} from 'react';
 import {Link} from "react-router-dom";
 
 
-const Menu = () => {
+const Menu = (props) => {
   return (
-      <nav className='menu'>
-        <ul>
-          {/*Якоря (были раньше):*/}
-          <li>
-            <Link to='/users'>Пользователи</Link>
-          </li>
-          <li>
-            <Link to='/projects'>Проекты</Link>
-          </li>
-          <li>
-            <Link to='/todos'>Заметки</Link>
-          </li>
-        </ul>
-      </nav>
+    <nav className='menu'>
+      <ul>
+        {/*Якоря (были раньше):*/}
+        <li>
+          <Link className='menu-link' to='/users'>Пользователи</Link>
+        </li>
+        <li>
+          <Link className='menu-link' to='/projects'>Проекты</Link>
+        </li>
+        <li>
+          <Link className='menu-link' to='/todos'>Заметки</Link>
+        </li>
+        <li> {!props.is_authenticated() ?
+          <Link className='menu-link menu-login' to='/login'>Войти</Link> :
+          <span className='menu-link menu-logout' onClick={() => props.logout()}>Выйти</span>
+        }
+        </li>
+      </ul>
+    </nav>
   )
 }
 
@@ -28,7 +33,8 @@ export class Header extends PureComponent {
   render() {
     return (
       <header className='header'>
-        <Menu/>
+        <Menu is_authenticated={this.props.is_authenticated}
+              logout={this.props.logout}/>
       </header>
     )
   }
