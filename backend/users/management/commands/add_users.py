df76b47342e6b67c0c0321d64ba7a13e2d8b8f1e
Radif <mail@radif.ru>
2021-11-20 16:37:15 +0300

    upd: Теперь при использовании скрипта создания пользователей, он может попасть в несколько рандомных групп, а не 1 как раньше и не может попасть в группу Администраторов

--- backend/users/management/commands/add_users.py
+++ backend/users/management/commands/add_users.py
@@ -40,11 +40,18 @@ class Command(BaseCommand):
     def create_users(users: list[dict]) -> None:
         """ Создание пользователей
         :param users: список словарей с данными пользователей
-        roles_id список id существующих ролей в БД группы зависимостей
+        roles_id - список id существующих ролей в БД группы зависимостей, кроме
+        Администраторов
+        quantity_roles - количество ролей в БД, кроме Администраторов
+        rand_roles_id - случайные роли, кроме Администраторов
+        Пользователи создаются с рандомными ролями (но не администраторами!)
         """
         roles_id = [r_id['id'] for r_id in
-                    PermissionGroups.objects.all().values('id')]
+                    PermissionGroups.objects.all().values('id') if
+                    r_id['id'] != 1]
+        quantity_roles = len(roles_id)
         for user in users:
+            rand_roles_id = {*random.choices(roles_id, k=quantity_roles)}
             try:
                 if not User.objects.filter(username=user['name']):
                     User.objects.create_user(
@@ -54,7 +61,6 @@ class Command(BaseCommand):
                         middle_name=user['middle_name'],
                         email=user['email'],
                         birthdate=user['birthdate'],
-                        password=user['password']).roles.add(
-                        random.choice(roles_id))
+                        password=user['password']).roles.add(*rand_roles_id)
             except OperationalError or ProgrammingError as error:
                 print(f'\n{error}\n')
