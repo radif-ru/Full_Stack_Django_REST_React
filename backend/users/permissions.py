e4897837d6e1e1f96fde8b57048403b622498304
Radif <mail@radif.ru>
2021-11-20 21:23:24 +0300

    fix: Разрешил создавать аккаунты анонимам

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -13,13 +13,18 @@ class UserPermission(BasePermission):
         4. Создать аккаунт может любой, даже не авторизованный пользователь.
         5. Читать данные могут все. Но не все данные включены в сериализаторе
         """
-        if request.method == 'POST' or request.method == 'PUT' or \
-                request.method == 'PATCH' or request.method == 'DELETE':
+        if request.method == 'PUT' or request.method == 'PATCH' or \
+                request.method == 'DELETE':
             if request.user.is_authenticated:
                 if 'pk' in view.kwargs and request.user.pk == int(
                         view.kwargs['pk']):
                     return True
-                return request.user.roles.filter(
-                    role=Roles.ADMINISTRATOR) or request.user.is_superuser
+                elif request.user.roles.filter(
+                        role=Roles.ADMINISTRATOR) or request.user.is_superuser:
+                    return True
+            return False
+        if request.method == 'POST':
+            if request.user.is_anonymous:
+                return True
             return False
         return True
