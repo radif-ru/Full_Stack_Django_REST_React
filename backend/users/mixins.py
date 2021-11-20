3582ddf7fca468094369c297bfdab5063646fe29
Radif <mail@radif.ru>
2021-11-20 16:43:42 +0300

    create: Миксины удаления объекта. Вместо удаления поле в БД 'is_active' становится False (0)

---
+++ backend/users/mixins.py
@@ -0,0 +1,13 @@
+from rest_framework import status
+from rest_framework.response import Response
+
+
+class UserDestroyMixin:
+    def destroy(self, request, *args, **kwargs):
+        """ Переопределения метода удаления (DELETE)
+        Вместо фактического удаления меняем активность на 0 (False)
+        """
+        instance = self.get_object()
+        instance.is_active = 0
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
