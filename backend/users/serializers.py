0050e958b6d291ab08631f5ccec44fa5065119c0
Radif <mail@radif.ru>
2021-11-20 16:28:53 +0300

    fix: Исправлена проверка наличия 'birthdate' в аттрибутах

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -26,13 +26,14 @@ class UserModelSerializer(serializers.ModelSerializer):
         return user
 
     def validate(self, attrs):
-        # Разница дней между текущей датой и ДР
-        days = timezone.now().date() - attrs['birthdate']
-        # 365.2425 - в среднем дней в году по Григорианскому календарю
-        years = days / timedelta(days=365.2425)
-        if years < 18:
-            raise serializers.ValidationError(
-                'Школоте нельзя! Или укажи возраст больше 18 лет;)')
+        if 'birthdate' in attrs:
+            # Разница дней между текущей датой и ДР
+            days = timezone.now().date() - attrs['birthdate']
+            # 365.2425 - в среднем дней в году по Григорианскому календарю
+            years = days / timedelta(days=365.2425)
+            if years < 18:
+                raise serializers.ValidationError(
+                    'Школоте нельзя! Или укажи возраст больше 18 лет;)')
         return attrs
 
     class Meta:
