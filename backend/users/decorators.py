f5428734bdf9e80075b9e0add8d6b9882c584251
Radif <mail@radif.ru>
2021-11-25 19:25:07 +0300

    refactoring: проверка на возраст вынесена в отдельный файл с декораторами

---
+++ backend/users/decorators.py
@@ -0,0 +1,26 @@
+"""Декораторы пользователей"""
+import functools
+from datetime import timedelta
+
+from django.utils import timezone
+from rest_framework import serializers
+
+
+def is_young_serializer_validate_decorator(method):
+    """ Декоратор проверяет возраст пользователя.
+    На основе этого выдаёт разрешение на регистрацию
+    """
+
+    @functools.wraps(method)
+    def wrapper(self, attrs):
+        if 'birthdate' in attrs:
+            # Разница дней между текущей датой и ДР
+            days = timezone.now().date() - attrs['birthdate']
+            # 365.2425 - в среднем дней в году по Григорианскому календарю
+            years = days / timedelta(days=365.2425)
+            if years < 18:
+                raise serializers.ValidationError(
+                    'Школоте нельзя! Или укажи возраст больше 18 лет;)')
+        return attrs
+
+    return wrapper
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,10 +1,8 @@
-from datetime import timedelta
-
-from django.utils import timezone
 from rest_framework import serializers
 
 from projects.models import Project
 from todos.models import Todo
+from .decorators import is_young_serializer_validate_decorator
 from .models import User, PermissionGroups
 
 
@@ -33,15 +31,8 @@ class UserModelSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+    @is_young_serializer_validate_decorator
     def validate(self, attrs):
-        if 'birthdate' in attrs:
-            # Разница дней между текущей датой и ДР
-            days = timezone.now().date() - attrs['birthdate']
-            # 365.2425 - в среднем дней в году по Григорианскому календарю
-            years = days / timedelta(days=365.2425)
-            if years < 18:
-                raise serializers.ValidationError(
-                    'Школоте нельзя! Или укажи возраст больше 18 лет;)')
         return attrs
 
     class Meta:
