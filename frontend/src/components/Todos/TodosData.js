dabb2a2f731a766dbe9a4764c3f8f76b503050e9
Radif <mail@radif.ru>
2021-12-01 00:48:30 +0300

    create: Компонент данных заметок

--- frontend/src/components/Todos/TodosData.js
+++ frontend/src/components/Todos/TodosData.js
@@ -1,15 +1,20 @@
+import "./Todos.css"
+
 import {PureComponent} from "react";
 import {Link} from "react-router-dom";
 import dateFormat from "dateformat";
 
-export class TodosPage extends PureComponent {
+export class TodosData extends PureComponent {
 
   render() {
 
-    const {todos, users} = this.props
+    const {
+      todos, users, projects, login, isAuthenticated, deleteTodo
+    } = this.props
+    const user = users.find(user => user.username === login)
 
     return (
-      <div className="todos">
+      <div className="todos-data">
         {todos.map((todo, idx) =>
           <div key={idx}>
             <span className="comment">{todo.text}</span>
@@ -18,12 +23,25 @@ export class TodosPage extends PureComponent {
               <Link className="comment-user" to={`/users/${todo.user}`}>
                 {users.find(user => user.id === todo.user).username}
               </Link>
+
+              <span>=> </span>
+
+              <Link
+                className="comment-user"
+                to={`/projects/${projects.find(project =>
+                  project.id === todo.project).id}`
+                }
+              >
+                {projects.find(project => project.id === todo.project).name}
+              </Link>
+
               <span className="comment-datetime">
                 {dateFormat(
                   todo.created, "dddd, mmmm dS, yyyy, h:MM:ss TT"
                 )}
                 <span>. </span>
               </span>
+
               <span className="comment-datetime">
                 {todo.created !== todo.updated
                   ? `Обновлено: ${dateFormat(
@@ -31,6 +49,12 @@ export class TodosPage extends PureComponent {
                   : null
                 }
               </span>
+              {isAuthenticated() && todo.user === user.id
+                ? <span className="btn btn-outline-danger">
+                  <span onClick={() => deleteTodo(todo.id)}>Удалить!</span>
+                </span>
+                : null
+              }
             </div>
             <br/>
           </div>
