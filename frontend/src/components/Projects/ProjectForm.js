636abf5aac424a70fb56c871ec54b09b554f688c
Radif <mail@radif.ru>
2021-12-01 00:55:17 +0300

    create: Форма для создания проекта

---
+++ frontend/src/components/Projects/ProjectForm.js
@@ -0,0 +1,146 @@
+import "./Projects.css"
+
+import {PureComponent} from "react";
+
+/**
+ * Создание и обработка формы создания проекта
+ * При создании проекта пользователь обязательно попадает в список авторов,
+ * но может указать соавторов дополнительно
+ */
+export class ProjectForm extends PureComponent {
+  /**
+   * Прокидывание свойств (props) от родителя, начальное состояние текста
+   * @param props {object} Данные, переданные родителем
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      "name": "",
+      "repository": "",
+      "usersId": []
+    }
+  }
+
+  /**
+   * Присваивание состояний на основе события изменения поля ввода формы
+   *
+   * @param event {object} Событие, оттуда извлекаются данные проекта
+   */
+  handleChange(event) {
+    const {name, value} = event.target;
+    // Авторы
+    if (event.target.selectedOptions) {
+      let usersId = [];
+      for (let i = 0; i < event.target.selectedOptions.length; i++) {
+        usersId.push(+event.target.selectedOptions.item(i).value);
+      }
+      this.setState({
+        "usersId": usersId
+      });
+      return
+    }
+    // Имя и репозиторий
+    this.setState({
+      [name]: value
+    });
+  }
+
+  /**
+   * Присваивание состояний на основе события обработки отправки данных в форме
+   * К авторам добавляется создатель проекта
+   * @param event {object} Используется только для остановки события
+   */
+  handleSubmit(event) {
+    const {name, repository, usersId} = this.state;
+    const {users, login, createProject} = this.props;
+    const user = users.find(user => user.username === login);
+    const allUsersId = [...usersId, user.id]
+    createProject(name, repository, allUsersId);
+    this.setState({
+      "name": "",
+      "repository": "",
+      "users": "",
+    });
+    event.preventDefault();
+  }
+
+  /**
+   * Отображение формы. В качестве соавторов можно выбрать всех, кроме себя
+   * @returns {JSX.Element}
+   */
+  render() {
+    const {name, repository} = this.state;
+    const {users, login} = this.props;
+    const user = users.find(user => user.username === login);
+    const otherUsers = users.filter(el => el.id !== user.id);
+
+    return (
+      <form
+        onSubmit={(event => this.handleSubmit(event))}
+        className="row todo-form"
+      >
+        <legend>Создать проект:</legend>
+        <div className="col-3">
+          <input
+            required
+            type="text"
+            name="name"
+            maxLength="33"
+            placeholder="Имя"
+            aria-describedby="textHelpInline"
+            value={name}
+            className="form-control"
+            onChange={(event => this.handleChange(event))}
+          />
+          <span id="textHelpInline" className="form-text">
+            Введите имя проекта
+          </span>
+        </div>
+        <div className="col-4">
+          <input
+            required
+            type="url"
+            name="repository"
+            maxLength="99"
+            placeholder="url-адрес репозитория"
+            aria-describedby="textHelpInline"
+            value={repository}
+            className="form-control"
+            onChange={(event => this.handleChange(event))}
+          />
+          <span id="textHelpInline" className="form-text">
+            Введите url-адрес репозитория
+          </span>
+        </div>
+
+        <div className="col-3">
+          <select
+            multiple
+            name="usersId"
+            className="form-control form-select"
+            aria-describedby="textHelpSelect"
+            placeholder="Пользователи"
+            onChange={(event) =>
+              this.handleChange(event)
+            }
+          >
+            {otherUsers.map((item, idx) =>
+              <option value={item.id} key={idx}>
+                {item.username}
+              </option>)
+            }
+          </select>
+          <span id="textHelpSelect" className="form-text">
+            Добавьте тех, кто так же работают с проектом
+          </span>
+        </div>
+
+        <input
+          type="submit"
+          value="Сохранить"
+          className="auth-btn btn btn-primary col-2"
+        />
+      </form>
+    )
+  }
+}
