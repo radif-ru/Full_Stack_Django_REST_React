5ebac0235604650ccaca0ed8ad3e49b0057f8d9f
Radif <mail@radif.ru>
2021-12-03 14:38:01 +0300

    upd: Уникальность проекта теперь определяется одновременно по имени и репозиторию

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -6,8 +6,7 @@ from users.models import User
 class Project(models.Model):
     """Модель проекта, для которого записаны заметки"""
     name = models.CharField(
-        verbose_name='название', max_length=33, blank=False, null=False,
-        unique=True)
+        verbose_name='название', max_length=33, blank=False, null=False)
     repository = models.URLField(
         verbose_name='репозиторий (url-адрес)', max_length=99, default='')
     users = models.ManyToManyField(User, verbose_name='работают с проектом',
@@ -25,3 +24,4 @@ class Project(models.Model):
         verbose_name = 'Проект'
         verbose_name_plural = 'Проекты'
         ordering = ['is_active', '-created', 'name']
+        unique_together = (('name', 'repository'),)
