263ac978dc40866e2a9dcc9d8c49940f4ece66d1
Radif <mail@radif.ru>
2021-12-20 12:15:31 +0300

    upd: asyncio.gather

--- backend/asyncIO-fish-text.py
+++ backend/asyncIO-fish-text.py
@@ -1,12 +1,16 @@
 import asyncio
 import json
-
 from requests import request
+from contextvars import ContextVar
+
+# Создание контекстной переменной для глобального доступа
+Texts = ContextVar('texts', default=[])
 
 
-def callback(result):
-    """Обработка результата выполнения задачи"""
-    print(result.result())
+# def callback(result):
+#     """Обработка результата выполнения задачи"""
+#     text = result.result()
+#     print(text)
 
 
 async def get_fish_text(num):
@@ -17,19 +21,30 @@ async def get_fish_text(num):
     status = dict_data['status']
     text = dict_data['text']
 
+    texts = Texts.get()
     if status == 'success':
-        return {num: text}
+        # return {num: text}
+        texts.append({num: text})
     else:
-        return {num: f'Ошибка! {status}'}
+        # return {num: f'Ошибка! {status}'}
+        texts.append({num: text})
+    Texts.set(texts)
 
 
 async def main():
     """Создание асинхронных задач и обработка результатов выполнения"""
-    for num in range(1, 11):
-        task = event_loop.create_task(get_fish_text(num))
-        task.add_done_callback(callback)
+    # for num in range(1, 11):
+    #     task = event_loop.create_task(get_fish_text(num))
+    #     task.add_done_callback(callback)
+
+    await asyncio.gather(*(get_fish_text(num) for num in range(1, 11)))
+    # tasks.add_done_callback(callback)
+
+    texts = Texts.get()
+    print(texts)
 
 
-event_loop = asyncio.get_event_loop()
-event_loop.run_until_complete(main())
-event_loop.run_forever()
+# event_loop = asyncio.get_event_loop()
+# event_loop.run_until_complete(main())
+# event_loop.run_forever()
+asyncio.run(main())
